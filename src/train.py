80fb2e4400ef64490b9e8fe7e8bf039f3e872752
Tooba Ali <toobaali@Toobas-MacBook-Air.local>
2025-09-20 17:09:28 -0400

    Change the model to Ramdom Forest Classifier

--- src/train.py
+++ src/train.py
@@ -1,19 +1,24 @@
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
 import joblib
 from data import load_data, split_data
 
 def fit_model(X_train, y_train):
     """
-    Train a Decision Tree Classifier and save the model to a file.
+    Train a Random Forest Classifier and save the model to a file.
     Args:
         X_train (numpy.ndarray): Training features.
         y_train (numpy.ndarray): Training target values.
     """
-    dt_classifier = DecisionTreeClassifier(max_depth=5, random_state=9)
-    dt_classifier.fit(X_train, y_train)
-    joblib.dump(dt_classifier, "../model/iris_model.pkl")
+    rf_classifier = RandomForestClassifier(
+        n_estimators=100,   # number of trees
+        max_depth=5,        # control tree depth
+        random_state=9
+    )
+    rf_classifier.fit(X_train, y_train)
+    joblib.dump(rf_classifier, "../model/iris_model.pkl")
 
 if __name__ == "__main__":
     X, y = load_data()
     X_train, X_test, y_train, y_test = split_data(X, y)
     fit_model(X_train, y_train)
+
